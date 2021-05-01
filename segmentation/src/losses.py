9f637f9dcdb5407a29467dfb91c98c2859be6bc3
Hyerin-oh <68813518+Hyerin-oh@users.noreply.github.com>
2021-05-01 17:18:00 +0900

    Update losses.py

--- segmentation/src/losses.py
+++ segmentation/src/losses.py
@@ -107,35 +107,20 @@ class IoULoss(nn.Module):
 
 
 class FocalLoss(nn.Module):
-    def __init__(self, gamma=0, alpha=None, size_average=True):
+    def __init__(self, gamma=2, alpha=.25, eps=1e-7, weights=None):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
         self.alpha = alpha
-        if isinstance(alpha,(float,int)): self.alpha = torch.Tensor([alpha,1-alpha])
-        if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
-        self.size_average = size_average
+        self.eps = eps
+        self.weight = weights
 
-    def forward(self, input, target):
-        if input.dim()>2:
-            input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
-            input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
-            input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
-        target = target.view(-1,1)
+    def forward(self, inp, tar):
+        logp = F.log_softmax(inp, dim=1)
+        ce_loss = F.nll_loss(logp, tar, weight=self.weight, reduction='none')
+        pt = torch.exp(-ce_loss)
 
-        logpt = F.log_softmax(input)
-        logpt = logpt.gather(1,target)
-        logpt = logpt.view(-1)
-        pt = Variable(logpt.data.exp())
-
-        if self.alpha is not None:
-            if self.alpha.type()!=input.data.type():
-                self.alpha = self.alpha.type_as(input.data)
-            at = self.alpha.gather(0,target.data.view(-1))
-            logpt = logpt * Variable(at)
-
-        loss = -1 * (1-pt)**self.gamma * logpt
-        if self.size_average: return loss.mean()
-        else: return loss.sum()
+        loss = self.alpha * (1 - pt) ** self.gamma * ce_loss
+        return loss.mean()
 
 class TverskyLoss(nn.Module):
     def __init__(self, alpha=0.5, beta=0.5, weight=None, size_average=True):

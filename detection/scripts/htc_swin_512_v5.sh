68ae9bedb4cff0ef42c3d71dd2d7b30453e992fe
MaiHon <mai.hong0924@gmail.com>
2021-05-22 23:23:38 +0900

    detection add

--- detection/scripts/htc_swin_512_v5.sh
+++ detection/scripts/htc_swin_512_v5.sh
@@ -1,5 +1,5 @@
 python src/train.py --cfg_path './src/configs/swin/swin_detection_strong_fpn.py' \
-                    --exp_name 'swin_test' --img_prefix './input/data' \
-                    --batch_size 1 \
-                    --img_res 256 \
+                    --exp_name 'htc_swin_v5' --img_prefix './input/data' \
+                    --batch_size 4 \
+                    --img_res 512 \
                     --cfg_options model.pretrained='./src/pretrained/swin_base_patch4_window12_384_22kto1k.pth'
--- detection/test_scripts/swin_test.sh
+++ detection/test_scripts/swin_test.sh
@@ -1,6 +1,5 @@
-python src/test.py --cfg_path './src/configs/swin/swin_detection.py' \
-                    --exp_name 'swin_B_384' --img_prefix './input/data' \
-                    --batch_size 8 \
+python src/test.py --cfg_path './src/configs/swin/swin_detection_strong_fpn.py' \
+                    --exp_name 'htc_swin_v9' --img_prefix './input/data' \
                     --img_res 512 \
-                    --ckpt "epoch_36" \
-                    --name "swin_test"
+                    --ckpt "epoch_40" \
+                    --name "pseudo_cascade_rcnn_swin_v9"
